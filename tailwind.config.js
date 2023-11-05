846f7676d4ea83396125843a9a728ec352f87586
Dec3ptor <benfoote90@gmail.com>
2023-11-06 01:18:01 +1300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js}", // Adjust the path according to your project structure
+    "./*.{html,js}", // Adjust the path according to your project structure
   ],
   theme: {
     extend: {},
